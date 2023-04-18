a728cf9ea8a5ed2cc0b06b25b31203360b284356
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-04-18 18:12:18 +0530

    fix dp function

--- src/utils/dp.ts
+++ src/utils/dp.ts
@@ -2,7 +2,8 @@ import consts from "@/consts";
 
 const dp = (pic?: string) => {
     if (pic) {
-        if (pic.includes("/static/")) pic = consts.API_URL + pic;
+        if (pic.includes("/static/") && !pic.includes(consts.API_URL))
+            pic = consts.API_URL + pic;
     } else {
         pic = "/usr_profile_pic.svg";
     }
