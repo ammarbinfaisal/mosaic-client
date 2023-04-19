4a4e42f77924ef28ab3633ad41fd3c87cb2705c6
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-04-19 15:11:57 +0530

    update search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,32 +1,80 @@
-import React, { useState } from "react";
+import consts from "@/consts";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const Search = () => {
     const [search, setSearch] = useState("");
-    // const [results, setResults] = useState([]);
-    // const [loading, setLoading] = useState(false);
+    const [results, setResults] = useState<any>({});
 
-    const searchQuery = async () => {
-        // setLoading(true);
-        // const response = await fetch(`/api/search?q=${search}`);
-        // const data = await response.json();
-        // setResults(data);
-        // setLoading(false);
-    };
+    useEffect(() => {
+        if (search.length === 0) {
+            setResults({});
+            return;
+        }
+        fetch(`${consts.API_URL}/search?q=${search}`).then(async (response) => {
+            const data = await response.json();
+            setResults(data);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }, [search]);
 
     const onChange = (e: any) => {
-        setSearch(e.target.value);
-        searchQuery();
+        setSearch(e.target.value.trim());
     };
 
-    // return (
-    //     <input
-    //         type="text"
-    //         value={search}
-    //         onChange={onChange}
-    //         className="mx-auto w-3/4 text-gray-700 placeholder-gray-400 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent border-2 border-stone-400 text-lg bg-stone-300"
-    //     />
-    // );
-    return null;
+    return (
+        <div className="flex flex-row items-center justify-between w-full relative">
+            <input
+                type="text"
+                value={search}
+                onChange={onChange}
+                className="mx-auto w-full text-gray-700 placeholder-gray-400 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent border-2 border-stone-400 text-lg bg-stone-100"
+            />
+            {results?.users?.length || results?.communities?.length ? (
+                <div className="absolute bg-white rounded-lg shadow-lg overflow-hidden max-h-96 top-16 w-full">
+                    <div className="flex flex-col items-start justify-between px-4 py-2 bg-gray-100">
+                        <h2 className="text-gray-900 font-semibold">Users</h2>
+                        {results?.users?.length
+                            ? results?.users?.map((user: any) => (
+                                  <div
+                                      className="flex flex-row items-center justify-between px-4 py-2 hover:bg-gray-100"
+                                      key={user.id}
+                                  >
+                                      <Link
+                                          href={`/u/${user.username}`}
+                                          className="text-gray-900"
+                                      >
+                                          u/{user.username}
+                                      </Link>
+                                  </div>
+                              ))
+                            : null}
+                    </div>
+                    <div className="flex flex-col items-start justify-between px-4 py-2 bg-gray-100">
+                        <h2 className="text-gray-900 font-semibold">
+                            Communities
+                        </h2>
+                        {results?.communities?.length
+                            ? results?.communities?.map((comm: any) => (
+                                  <div
+                                      className="flex flex-row items-center justify-between px-4 py-2 hover:bg-gray-100"
+                                      key={comm.id}
+                                  >
+                                      <Link
+                                          href={`/c/${comm.name}`}
+                                          className="text-gray-900"
+                                      >
+                                          c/{comm.name}
+                                      </Link>
+                                  </div>
+                              ))
+                            : null}
+                    </div>
+                </div>
+            ) : null}
+        </div>
+    );
 };
 
 export default Search;
