40ca6a1a46d09fe026f700bd30e0744e1b735fb1
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-04-25 19:29:22 +0530

    improve comment page

--- src/pages/cm/[id].tsx
+++ src/pages/cm/[id].tsx
@@ -2,6 +2,7 @@ import Comment from "@/components/Comment";
 import consts from "@/consts";
 import Head from "next/head";
 import Main from "@/components/Main";
+import Navbar from "@/components/Navbar";
 
 const CommentPage = ({ comment }: any) => {
     return (
@@ -10,6 +11,8 @@ const CommentPage = ({ comment }: any) => {
                 <title>{comment.content.slice(0, 20)}</title>
             </Head>
             <Main>
+                <Navbar search/>
+                <hr className="my-4 bg-transparent" />
                 <Comment comment={comment} isPage depth={0} />
             </Main>
         </>
