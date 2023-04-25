940d4bc2cb0ded0355dc9895b267984a5915d845
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-04-25 21:23:57 +0530

    fix feed sorting

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -12,31 +12,15 @@ const Feed = ({ sort, home }: any) => {
         fetcher(1000 * 60 * 5)
     );
     const { data: trending } = useSWR("trending", fetcher(1000 * 60 * 5));
-    const [posts, setPosts] = useState<any>([]);
+    const [posts, setPosts] = useState<any>(home ? h : trending);
 
     useEffect(() => {
-        let sorted: any = home ? h : trending;
-        sorted?.sort((a: any, b: any) => {
-            let bcreat = new Date(b.time_created);
-            let acreat = new Date(a.time_created);
-            if (sort == Sort.hot) {
-                return (a.upvotes + a.downvotes) / (Date.now() - +acreat) >
-                    (b.upvotes + b.downvotes) / (Date.now() - +bcreat)
-                    ? -1
-                    : 1;
-            }
-            if (sort == Sort.new) {
-                return +acreat > +bcreat ? 1 : -1;
-            }
-            if (sort == Sort.top) {
-                return (a.upvotes - a.downvotes) > (b.upvotes - b.downvotes)
-            }
-            if (sort == Sort.old) {
-                return +acreat > +bcreat ? -1 : 1;
-            }
-        });
-        setPosts(sorted);
-    }, [h, home, sort, trending]);
+        if (home) {
+            setPosts(h);
+        } else {
+            setPosts(trending);
+        }
+    }, [home, h, trending]);
 
     if (home) {
         if (!auth.isLoggedIn) return <p>login to see your feed</p>;
@@ -45,7 +29,27 @@ const Feed = ({ sort, home }: any) => {
     return (
         <div className="w-full">
             {posts &&
-                posts.map((post: any) => (
+                posts.sort((a: any, b: any) => {
+                    let bcreat = new Date(b.time_created);
+                    let acreat = new Date(a.time_created);
+                    if (sort == Sort.hot) {
+                        return (a.upvotes + a.downvotes) / (Date.now() - +acreat) >
+                            (b.upvotes + b.downvotes) / (Date.now() - +bcreat)
+                            ? -1
+                            : 1;
+                    }
+                    if (sort == Sort.new) {
+                        return +acreat > +bcreat ? -1 : 1;
+                    }
+                    if (sort == Sort.top) {
+                        const av = a.upvotes - a.downvotes;
+                        const bv = b.upvotes - b.downvotes;
+                        return bv - av;
+                    }
+                    if (sort == Sort.old) {
+                        return +acreat > +bcreat ? 1 : -1;
+                    }
+                }).map((post: any) => (
                     <PostFeed key={post.id} post={post} />
                 ))}
         </div>
