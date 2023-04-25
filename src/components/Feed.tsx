72efeba1d07c141c40aa8aeb42b0bfd002a723c6
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-04-25 20:40:50 +0530

    use separate sorted state

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -7,17 +7,36 @@ import useAuth from "@/hooks/useAuth";
 
 const Feed = ({ sort, home }: any) => {
     const auth = useAuth();
-    const { data: h } = useSWR(auth.isLoggedIn ? "me/feed" : null, fetcher(1000 * 60 * 5));
+    const { data: h } = useSWR(
+        auth.isLoggedIn ? "me/feed" : null,
+        fetcher(1000 * 60 * 5)
+    );
     const { data: trending } = useSWR("trending", fetcher(1000 * 60 * 5));
     const [posts, setPosts] = useState<any>([]);
 
     useEffect(() => {
-        if (home) {
-            setPosts(h);
-        } else {
-            setPosts(trending);
-        }
-    }, [h, home, trending]);
+        let sorted: any = home ? h : trending;
+        sorted.sort((a: any, b: any) => {
+            let bcreat = new Date(b.time_created);
+            let acreat = new Date(a.time_created);
+            if (sort == Sort.hot) {
+                return (a.upvotes + a.downvotes) / (Date.now() - +acreat) >
+                    (b.upvotes + b.downvotes) / (Date.now() - +bcreat)
+                    ? -1
+                    : 1;
+            }
+            if (sort == Sort.new) {
+                return +acreat > +bcreat ? -1 : 1;
+            }
+            if (sort == Sort.top) {
+                return a.upvotes > b.upvotes ? -1 : 1;
+            }
+            if (sort == Sort.old) {
+                return +acreat > +bcreat ? 1 : -1;
+            }
+        });
+        setPosts(sorted);
+    }, [h, home, sort, trending]);
 
     if (home) {
         if (!auth.isLoggedIn) return <p>login to see your feed</p>;
@@ -26,33 +45,9 @@ const Feed = ({ sort, home }: any) => {
     return (
         <div className="w-full">
             {posts &&
-                posts
-                    .sort((a: any, b: any) => {
-                        let bcreat = new Date(b.time_created);
-                        let acreat = new Date(a.time_created);
-                        if (sort == Sort.hot) {
-                            console.log("hot");
-                            return (a.upvotes + a.downvotes) /
-                                (Date.now() - +acreat) >
-                                (b.upvotes + b.downvotes) /
-                                    (Date.now() - +bcreat)
-                                ? -1
-                                : 1;
-                        }
-                        if (sort == Sort.new) {
-                            console.log("new");
-                            return +acreat > +bcreat ? -1 : 1;
-                        }
-                        if (sort == Sort.top) {
-                            console.log("top");
-                            return a.upvotes > b.upvotes ? -1 : 1;
-                        }
-                        if (sort == Sort.old) {
-                            console.log("old");
-                            return +acreat > +bcreat ? 1 : -1;
-                        }
-                    })
-                    .map((post: any) => <PostFeed key={post.id} post={post} />)}
+                posts.map((post: any) => (
+                    <PostFeed key={post.id} post={post} />
+                ))}
         </div>
     );
 };
