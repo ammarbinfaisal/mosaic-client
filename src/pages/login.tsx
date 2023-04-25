b0a48975cda19b6725e0071317aadbfcd02cf6c3
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-04-25 19:40:47 +0530

    fix login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import Image from "next/image";
 import useAuth from "@/hooks/useAuth";
 import Link from "next/link";
+import Main from "@/components/Main";
 
 const Login = () => {
     const [username, setUsername] = useState("");
@@ -44,7 +45,7 @@ const Login = () => {
             <Head>
                 <title>Mosaic Login</title>
             </Head>
-            <div className="h-screen w-screen grid grid-row-3 grid-cols-1 bg-light">
+            <Main>
                 <div className="flex flex-col items-center justify-center w-full row-span-2">
                     <Image
                         src="/logo@2x.png"
@@ -58,7 +59,7 @@ const Login = () => {
                         onSubmit={handleSubmit}
                     >
                         <input
-                            className="w-full px-4 py-2 placeholder:text-amber-900 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-800"
+                            className="w-full px-4 py-2 placeholder:text-stone-900 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-stome-800"
                             type="text"
                             placeholder="Username"
                             value={username}
@@ -66,7 +67,7 @@ const Login = () => {
                             onChange={(e) => setUsername(e.target.value)}
                         />
                         <input
-                            className="w-full px-4 py-2 placeholder:text-amber-900 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-800"
+                            className="w-full px-4 py-2 placeholder:text-stome-900 bg-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-stome-800"
                             type="password"
                             placeholder="Password"
                             value={password}
@@ -75,7 +76,7 @@ const Login = () => {
                         />
 
                         <button
-                            className="w-full px-4 py-2 bg-gray-100 rounded-md hover:bg-amber-900 hover:text-gray-100 transition-all"
+                            className="w-full px-4 py-2 bg-gray-100 rounded-md hover:bg-stome-900 hover:text-gray-100 transition-all"
                             type="submit"
                         >
                             Login
@@ -89,14 +90,7 @@ const Login = () => {
                     </Link>
                     {error && <p className="mt-4 text-red-800">{error}</p>}
                 </div>
-                <div className="w-full row-span-1 flex justify-center relative">
-                    <img
-                        src="/Bottom Waves.svg"
-                        alt="bottom waves"
-                        className="absolute bottom-0 scale-125"
-                    />
-                </div>
-            </div>
+            </Main>
         </>
     );
 };
